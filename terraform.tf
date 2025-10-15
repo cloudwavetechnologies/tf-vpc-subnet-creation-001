67c1106e54f5943caa77cd5ab23824d1a7027422
cloudwavetechnologies <manjunath@cloudwavetechnologies.co.in>
2025-10-15 07:59:30 +0530

    added subnet tf file

--- terraform.tf
+++ terraform.tf
@@ -10,7 +10,9 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
-
+##################################################
+# VPC Creation
+##################################################
 resource "aws_vpc" "main_vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
@@ -18,5 +20,18 @@ resource "aws_vpc" "main_vpc" {
 
   tags = {
     Name = var.vpc_name
+  }
+}
+##################################################
+# Subnet Creation
+##################################################
+resource "aws_subnet" "main_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = var.subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = var.subnet_name
   }
 }
